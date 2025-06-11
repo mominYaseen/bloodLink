c8bae2655e832b21ca9be8b7366d89d3f21baa43
mominYaseen <moominyaseen786@gmail.com>
2025-06-11 17:51:46 +0530

    config updated

--- src/main/java/com/example/bloodLink/security/securityConfig/SecurityConfig.java
+++ src/main/java/com/example/bloodLink/security/securityConfig/SecurityConfig.java
@@ -1,4 +1,5 @@
 package com.example.bloodLink.security.securityConfig;
+
 import com.example.bloodLink.security.authenticationProvicer.JwtAuthenticationProvider;
 import com.example.bloodLink.security.filters.JwtAuthenticationFilter;
 import com.example.bloodLink.security.filters.JwtValidationFilter;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,70 +26,63 @@ import java.util.Arrays;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private JwtUtils jwtUtils;
+  private JwtUtils jwtUtils;
 
-    private UserDetailsService userDetailsService;
+  private UserDetailsService userDetailsService;
 
-    @Autowired
-    public SecurityConfig(JwtUtils jwtUtils, UserDetailsService userDetailsService) {
-        this.jwtUtils = jwtUtils;
-        this.userDetailsService = userDetailsService;
-    }
+  @Autowired
+  public SecurityConfig(JwtUtils jwtUtils, UserDetailsService userDetailsService) {
+    this.jwtUtils = jwtUtils;
+    this.userDetailsService = userDetailsService;
+  }
 
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider(){
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(passwordEncoder());
-        return provider;
-    }
+  @Bean
+  public DaoAuthenticationProvider daoAuthenticationProvider() {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+    provider.setUserDetailsService(userDetailsService);
+    provider.setPasswordEncoder(passwordEncoder());
+    return provider;
+  }
 
-    @Bean
-    public JwtAuthenticationProvider jwtAuthenticationProvider(){
-        return new JwtAuthenticationProvider(jwtUtils,userDetailsService);
-    }
+  @Bean
+  public JwtAuthenticationProvider jwtAuthenticationProvider() {
+    return new JwtAuthenticationProvider(jwtUtils, userDetailsService);
+  }
 
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http,
+      JwtUtils jwtUtils,
+      AuthenticationManager authenticationManager) throws Exception {
+    JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtUtils, authenticationManager);
+    JwtValidationFilter jwtValidationFilter = new JwtValidationFilter(authenticationManager);
 
+    http
+        .cors(Customizer.withDefaults())
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/user/register").permitAll()
+            .requestMatchers("/user/**").hasRole("USER")
+            .requestMatchers("/admin/**").hasRole("SUB_ADMIN")
+            .requestMatchers("/super-admin/**").hasRole("SUPER_ADMIN"))
+        .sessionManagement(session -> session
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
+        )
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+        .addFilterAfter(jwtValidationFilter, JwtAuthenticationFilter.class)
+        .csrf(csrf -> csrf.disable());
+    return http.build();
+  }
 
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder(12);
+  }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http ,
-                                                   JwtUtils jwtUtils ,
-                                                   AuthenticationManager authenticationManager) throws Exception
-    {
-        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtUtils,authenticationManager);
-        JwtValidationFilter jwtValidationFilter = new JwtValidationFilter(authenticationManager);
-
-        http.authorizeHttpRequests(auth->auth
-                        .requestMatchers("/user/register").permitAll()
-                        .requestMatchers("/user/**").hasRole("USER")
-                        .requestMatchers("/admin/**").hasRole("SUB_ADMIN")
-                        .requestMatchers("/super-admin/**").hasRole("SUPER_ADMIN")
-                )
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
-                )
-                .addFilterBefore(jwtAuthenticationFilter , UsernamePasswordAuthenticationFilter.class)
-                .addFilterAfter(jwtValidationFilter , JwtAuthenticationFilter.class)
-                .csrf(csrf->csrf.disable());
-        return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder()
-    {
-        return new BCryptPasswordEncoder(12);
-    }
-
-
-    @Bean
-    public AuthenticationManager authenticationManager(){
-        return new ProviderManager(Arrays.asList(
-                daoAuthenticationProvider(),
-                jwtAuthenticationProvider()
-        ));
-    }
+  @Bean
+  public AuthenticationManager authenticationManager() {
+    return new ProviderManager(Arrays.asList(
+        daoAuthenticationProvider(),
+        jwtAuthenticationProvider()));
+  }
 
 }
